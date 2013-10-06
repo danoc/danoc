19eabef56d42ec131b05413a1625802affecdc40
Daniel O'Connor <daniel@danoc.me>
2013-10-06 10:03:49 -0400

    danoc.me redesign

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+parseRSS('http://blog.danoc.me/rss', function(e) {
+	var entries = e.entries;
+	var m_names = new Array("Jan.", "Feb.", "Mar.",
+	"Apr.", "May", "June", "July", "Aug.", "Sept.",
+	"Oct.", "Nov.", "Dec.");
+	
+	for(var i = 0; i < 5; i++) {
+		var published = new Date(entries[i].publishedDate);
+		var post = '<li><h3 class="post-title"><a href="' + entries[i].link + '">' + entries[i].title + '</a><time class="post-date">'+ m_names[published.getMonth()] + ' ' + published.getDate() +'</time></h3></li>';
+		$('#posts-list').append(post);
+	}
+});
--- js/plugins.js
+++ js/plugins.js
@@ -1,35 +1,32 @@
 // Avoid `console` errors in browsers that lack a console.
-if (!(window.console && console.log)) {
-    (function() {
-        var noop = function() {};
-        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
-        var length = methods.length;
-        var console = window.console = {};
-        while (length--) {
-            console[methods[length]] = noop;
-        }
-    }());
-}
+(function() {
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
 
-// Place any jQuery/helper plugins in here.
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+}());
+
 function parseRSS(url, callback) {
   $.ajax({
-    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-    dataType: 'json',
-    success: function(data) {
-      callback(data.responseData.feed);
-    }
+	url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+	dataType: 'json',
+	success: function(data) {
+	  callback(data.responseData.feed);
+	}
   });
 }
-
-parseRSS('http://blog.danoc.me/rss', function(e) {
-	var entries = e.entries;
-	var m_names = new Array("January", "February", "March",
-	"April", "May", "June", "July", "August", "September",
-	"October", "November", "December");
-	
-	for(var i = 0; i < 3; i++) {
-		var published = new Date(entries[i].publishedDate);
-		$('#blog .posts').append('<article class="entry"><h3 class="title"><a href="'+entries[i].link+'">'+entries[i].title+'</a></h3> &mdash; '+ m_names[published.getMonth()] + ' ' + published.getDate() +'</article>');
-	}
-});

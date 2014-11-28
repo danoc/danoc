c1745fd963f43b9afb303fa988ce2d25f97b9d7d
Daniel O'Connor <daniel@danoc.me>
2014-11-28 12:44:48 -0500

    Use LESS and gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function() {});
+
+var bower = require('gulp-bower');
+
+gulp.task('bower', function() {
+  return bower()
+  .pipe(gulp.dest('lib/'))
+});
+
+gulp.task('less', function () {
+  gulp.src('./assets/less/*.less')
+  .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./public/css'));
 });

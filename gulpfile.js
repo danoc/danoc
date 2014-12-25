1ba951b7906786e0a336de5d4bcaca0960ac317b
Daniel O'Connor <daniel@optimizely.com>
2014-12-24 23:33:32 -0500

    Many improvements to the build system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,132 @@
 var gulp = require('gulp');
-var path = require('path');
 
+var autoprefixer = require('gulp-autoprefixer');
 var bower = require('gulp-bower');
+var browserSync = require('browser-sync');
+var childProcess = require('child_process');
 var del = require('del');
 var imagemin = require('gulp-imagemin');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
+var path = require('path');
+var plumber = require('gulp-plumber');
+var shell = require('gulp-shell');
+
+var assets = {
+  src: {
+    'css': ['./src/css/*.css', './lib/normalize.css/normalize.css'],
+    'img':  './src/img/**',
+    'less': './src/less/*.less'
+  },
+  dist: {
+    'css':  './_site/public/css',
+    'img':  './_site/public/img',
+    'less': './_site/public/less'
+  }
+};
+
+var jekyllPaths = ['index.html', '_layouts/*.html', '_posts/*'];
+
+
+/*=========================*/
+/*=== Gulp Default Task ===*/
+/*=========================*/
 
 // Runs when `gulp` runs
-gulp.task('default', ['less', 'css', 'img']);
+gulp.task('default', ['browserSync', 'less', 'css', 'img'], function() {
+  gulp.watch(assets['src']['css'], ['css']);
+  gulp.watch(assets['src']['img'], ['img']);
+  gulp.watch(assets['src']['less'], ['less']);
+  gulp.watch(jekyllPaths, ['jekyllRebuild']);
+});
+
+
+/*==========================*/
+/*=== Package Management ===*/
+/*==========================*/
 
 // Install the bower dependencies
-gulp.task('bower', ['clean'], function() {
+gulp.task('bower', function() {
   return bower()
     .pipe(gulp.dest('lib/'))
 });
 
+
+/*==============*/
+/*=== Assets ===*/
+/*==============*/
+
 // Delete the existing gulp files
 gulp.task('clean', function(cb) {
-  return del(['public'], cb);
+  return del(['_site'], cb);
 });
 
 // Compile LESS and minify
-gulp.task('less', ['bower'], function () {
-  return gulp.src('./assets/less/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+gulp.task('less', ['bower'], function(event) {
+  return gulp.src(assets['src']['less'])
+    .pipe(less().on('error', function(state) {
+      console.error(state);
+      browserSync.notify(state.message, 3000);
+      this.end();
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest(assets['dist']['css']));
 });
 
 // Minify CSS files
-gulp.task('css', ['bower'], function() {
-  return gulp.src(['./assets/css/*.css', './lib/normalize.css/normalize.css'])
+gulp.task('css', ['bower'], function(event) {
+  return gulp.src(assets['src']['css'])
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest(assets['dist']['css']));
 });
 
 // Minify image files
-gulp.task('img', ['bower'], function() {
-  return gulp.src('./assets/img/**')
+gulp.task('img', ['bower'], function(event) {
+  return gulp.src(assets['src']['img'])
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
     }))
-    .pipe(gulp.dest('./public/img'));
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest(assets['dist']['img']));
+});
+
+
+/*===================*/
+/*=== browserSync ===*/
+/*===================*/
+
+// Start browserSync to view the website
+gulp.task('browserSync', ['jekyllBuild'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+
+/*==============*/
+/*=== Jekyll ===*/
+/*==============*/
+
+// Build the Jekyll website
+gulp.task('jekyllBuild', ['clean'], function(done) {
+  return childProcess.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+// Rebuild the Jekyll website
+gulp.task('jekyllRebuild', ['jekyllBuild'], function() {
+  browserSync.reload();
 });

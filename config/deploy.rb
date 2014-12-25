ebe74ed9e4bd287e81aa699faef6e6cc32deb172
Daniel O'Connor <daniel@optimizely.com>
2014-12-25 02:23:50 -0500

    Separate the build commands into multiple.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,15 @@ lock '3.3.5'
 set :application, 'danoc.me'
 set :repo_url, 'git@github.com:danoc/danoc.me.git'
 
+set :log_level, :info
 
 namespace :deploy do
 
   before :publishing, :build do
     on roles(:all) do
-      execute "cd #{release_path} && npm install --production --silent --no-spin && ./node_modules/.bin/gulp jekyllBuild"
+      execute "cd #{release_path} && bundle install"
+      execute "cd #{release_path} && npm install --production --silent --no-spin"
+      execute "cd #{release_path} && ./node_modules/.bin/gulp jekyllBuild"
     end
   end
 

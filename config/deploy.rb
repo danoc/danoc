e45e25d11bcb7380d467eab7525b3e90632fa8c7
Daniel O'Connor <daniel@optimizely.com>
2015-03-01 11:54:10 -0800

    Improvements to the deploy process.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,14 +6,13 @@ lock '3.3.5'
 set :application, 'danoc.me'
 set :repo_url, 'git@github.com:danoc/danoc.me.git'
 
-set :pty, true
 set :log_level, :info
 
 namespace :deploy do
 
   before :publishing, :build do
     on roles(:all) do
-      execute "cd #{release_path} && bundle install"
+      execute "cd #{release_path} && sudo bundle install"
       execute "cd #{release_path} && npm install --production --silent --no-spin"
       execute "cd #{release_path} && ./node_modules/.bin/gulp jekyllBuild"
     end

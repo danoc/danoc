d61f8a35f399c677ad47b8a795d3bcf26f62f3c4
Daniel O'Connor <daniel@danoc.me>
2020-01-18 21:51:19 -0800

    Use the reusable paragraph component.

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -12,16 +12,7 @@ import UntitledSansRegularWoff from "../fonts/UntitledSansWeb-Regular.woff";
 import UntitledSansRegularWoff2 from "../fonts/UntitledSansWeb-Regular.woff2";
 import UntitledSansRegularItalicWoff from "../fonts/UntitledSansWeb-RegularItalic.woff";
 import UntitledSansRegularItalicWoff2 from "../fonts/UntitledSansWeb-RegularItalic.woff2";
-
-type ParagraphProps = {
-  children: React.ReactNode;
-};
-
-const Paragraph = ({ children }: ParagraphProps) => (
-  <p css={{ marginTop: s.s0, maxWidth: s.measure, lineHeight: s.lhCopy }}>
-    {children}
-  </p>
-);
+import Paragraph from "./paragraph";
 
 type ContainerProps = {
   children: React.ReactNode;

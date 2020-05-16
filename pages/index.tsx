47f6ad684b8d64870eb0439a841b69c40b9c25ab
Daniel O'Connor <daniel@danoc.me>
2020-05-16 08:51:28 -0700

    Add fonts and some more styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
 function HomePage() {
   return (
-    <div className="font-sans">
-      <span>👨‍💻</span>
-      <header>Daniel O’Connor</header>
-      <p>
-        Hello! I’m a design systems and front-end infrastructure engineer in San
-        Francisco. I use code and communication to improve product quality and
-        developer productivity.
-      </p>
-      <p>
-        Right now I build <a href="https://thumbprint.design/">Thumbprint</a>,
-        the design system at <a href="https://www.thumbtack.com/">Thumbtack</a>.
-        I previously worked at{" "}
-        <a href="https://www.optimizely.com/">Optimizely</a> where I helped
-        build and maintain <a href="https://github.com/optimizely/oui">OUI</a>,
-        a React component library.
-      </p>
+    <div className="font-sans max-w-screen-sm mx-auto">
+      <header>
+        <span className="text-4xl">👨‍💻</span>
+        <h1 className="text-2xl font-medium">Daniel O’Connor</h1>
+        <p>
+          Hello! I’m a design systems and front-end infrastructure engineer in
+          San Francisco. I use code and communication to improve product quality
+          and developer productivity.
+        </p>
+        <p>
+          Right now I build <a href="https://thumbprint.design/">Thumbprint</a>,
+          the design system at{" "}
+          <a href="https://www.thumbtack.com/">Thumbtack</a>. I previously
+          worked at <a href="https://www.optimizely.com/">Optimizely</a> where I
+          helped build and maintain{" "}
+          <a href="https://github.com/optimizely/oui">OUI</a>, a React component
+          library.
+        </p>
+      </header>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./layouts/**/*.{js,jsx,ts,tsx}",
     "./utils/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans:
+        '"Untitled Sans", -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica, helvetica neue, ubuntu, roboto, noto, segoe ui, arial, sans-serif',
+    },
+  },
   variants: {},
   plugins: [],
 };

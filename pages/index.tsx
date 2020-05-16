023724735a4df16e6b709257912acb93f383baf0
Daniel O'Connor <daniel@danoc.me>
2020-05-16 07:53:02 -0700

    Rewrite site in Next.js and Tailwind.

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+function HomePage() {
+  return (
+    <div className="font-sans">
+      <span>👨‍💻</span>
+      <header>Daniel O’Connor</header>
+      <p>
+        Hello! I’m a design systems and front-end infrastructure engineer in San
+        Francisco. I use code and communication to improve product quality and
+        developer productivity.
+      </p>
+      <p>
+        Right now I build <a href="https://thumbprint.design/">Thumbprint</a>,
+        the design system at <a href="https://www.thumbtack.com/">Thumbtack</a>.
+        I previously worked at{" "}
+        <a href="https://www.optimizely.com/">Optimizely</a> where I helped
+        build and maintain <a href="https://github.com/optimizely/oui">OUI</a>,
+        a React component library.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;

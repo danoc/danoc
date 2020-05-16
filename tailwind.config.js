b129028e97fa9e05452826fd44976ac25fda3d07
Daniel O'Connor <daniel@danoc.me>
2020-05-16 08:38:35 -0700

    Fix build and deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./layouts/**/*.{js,jsx,ts,tsx}",
+    "./utils/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+};

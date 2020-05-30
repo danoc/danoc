a3bfece818d2c91fb8a72a4d775275cb4d522dca
Daniel O'Connor <daniel@danoc.me>
2020-05-30 16:14:38 -0700

    Remove offline plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
     },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-feed",

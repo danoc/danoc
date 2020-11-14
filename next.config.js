5b38793f7b522c726d1f5bd994439826e4ca973f
Daniel O'Connor <daniel@danoc.me>
2020-11-14 15:36:49 -0500

    Get images rendering in posts.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+const copyLinkedFiles = require("remark-copy-linked-files");
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      [
+        copyLinkedFiles,
+        {
+          destinationDir: "./public/mdx",
+          staticPath: "mdx",
+        },
+      ],
+    ],
+  },
 });
 
 module.exports = withMDX();

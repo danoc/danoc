4b36af675136c486f4bb32cb3cbedb0b9fbaba14
Daniel O'Connor <daniel@danoc.me>
2020-12-18 21:30:55 -0500

    Add a footer

--- components/container/index.tsx
+++ components/container/index.tsx
@@ -1,7 +1,34 @@
+import { DottedLink } from "../link";
+
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 export default function Container({ children }: ContainerProps) {
-  return <div className="p-5 max-w-xl mx-auto">{children}</div>;
+  return (
+    <div className="p-5 max-w-xl mx-auto">
+      {children}
+
+      <footer className="pt-4 border-t border-gray-200">
+        <ul className="inline-grid gap-y-4 gap-x-2">
+          <li className="col-span-3">
+            <DottedLink href="mailto:daniel@danoc.me">
+              daniel@danoc.me
+            </DottedLink>
+          </li>
+          <li className="text-sm">
+            <DottedLink href="https://twitter.com/_danoc">Twitter</DottedLink>
+          </li>
+          <li className="text-sm">
+            <DottedLink href="https://github.com/danoc">GitHub</DottedLink>
+          </li>
+          <li className="text-sm">
+            <DottedLink href="https://linkedin.com/in/itsdanoc">
+              LinkedIn
+            </DottedLink>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
 }
